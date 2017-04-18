d10309809f66e242faa51daab1e27fa66c37dab5
jjromphf <jromphf@library.rochester.edu>
2017-04-18 17:39:09 -0400

    speed test

--- controls.py
+++ controls.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
     controls.motorSetup()
 
     for step in range(0, 100):
-        time.sleep(0.10)
+        time.sleep(0.05)
         controls.motorForward()
 
     GPIO.output(controls.rightStepper['enable'], GPIO.HIGH)
