8ad638b968f44700480a07df5a2e465a72e1c483
jjromphf <jromphf@library.rochester.edu>
2017-04-18 18:59:14 -0400

    trying infinite loop

--- controls.py
+++ controls.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
     controls.motorSetup()
     while True:
         for step in range(0, 1000):
-            time.sleep(0.001)
+            time.sleep(0.004)
             controls.motorForward()
 
     GPIO.output(controls.rightStepper['enable'], GPIO.HIGH)
