a1921a2baadbf4fd04e275e0b692c1d61a7f94a9
jjromphf <jromphf@library.rochester.edu>
2017-04-18 17:39:54 -0400

    speed test

--- controls.py
+++ controls.py
@@ -85,7 +85,7 @@ class Controls(object):
 
     def forward(self):
 
-        for i in range(50):
+        for i in range(20):
             GPIO.output(self.leftStepper['step'], GPIO.HIGH)
             time.sleep(0.0001)
             GPIO.output(self.leftStepper['step'], GPIO.LOW)
